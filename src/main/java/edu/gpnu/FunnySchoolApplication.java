ec13e30892570f41eb067f82c1615f0b7b36b22b
yanchenhao <2865366928@qq.com>
2020-05-29 16:02:50 +0800

    测试git

--- src/main/java/edu/gpnu/FunnySchoolApplication.java
+++ src/main/java/edu/gpnu/FunnySchoolApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FunnySchoolApplication {
 
     public static void main(String[] args) {
+        //测试修改
         SpringApplication.run(FunnySchoolApplication.class, args);
     }
 
